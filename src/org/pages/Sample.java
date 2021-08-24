63442df6222b2c3dcd9225b6547810f7272eeb56
DhilipanAnnadurai <dhilipan20091995@gmail.com>
2021-08-24 23:20:43 +0530

    load Url

--- src/org/pages/Sample.java
+++ src/org/pages/Sample.java
@@ -2,4 +2,11 @@ package org.pages;
 
 public class Sample {
 
+	public static void loadUrl() {
+		System.out.println("Load Url");
+	}
+
+	public static void main(String[] args) {
+		loadUrl();
+	}
 }
