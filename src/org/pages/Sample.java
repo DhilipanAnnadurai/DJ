568faac078f558b724654eed927e2dd1ad605be0
DhilipanAnnadurai <dhilipan20091995@gmail.com>
2021-08-24 23:11:36 +0530

    getDriver

--- src/org/pages/Sample.java
+++ src/org/pages/Sample.java
@@ -2,4 +2,11 @@ package org.pages;
 
 public class Sample {
 
+	public static void getDriver() {
+		System.out.println("Get Driver");
+	}
+
+	public static void main(String[] args) {
+		getDriver();
+	}
 }
